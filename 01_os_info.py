a4665fb127b35f0f3d2ee6559c00491187251feb
denisitch <denisitch@gmail.com>
2020-11-16 21:07:56 +0300

    deleted files

--- 01_os_info.py
+++
@@ -1,8 +0,0 @@
-import platform
-import sys
-
-info = 'OS info is \n{}\n\nPython version is {} {}'.format(platform.uname(),
-    sys.version, platform.architecture())
-
-with open('os_info.txt', 'w') as ff:
-    ff.write(info)
